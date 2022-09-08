6c6563a6bd7c235e474a44ca313cb538f227dbd7
Thu Le <thule@Thus-MacBook-Pro-2.local>
2022-09-08 18:46:59 +0300

    rm path

--- bonus/config.py
+++ bonus/config.py
@@ -1,7 +1,7 @@
-PUSH_SWAP_PATH = '/Users/thule/Desktop/projects/push_swap/'
+PUSH_SWAP_PATH = ''
 PUSH_SWAP = PUSH_SWAP_PATH + 'push_swap'
 
-CHECKER_PATH = '/Users/thule/Desktop/projects/push_swap/'
+CHECKER_PATH = ''
 CHECKER = CHECKER_PATH + 'checker'
 
 # CONFIGURATION FOR VISUALIZER
